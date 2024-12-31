8c28aef3b47fb653deb1ff94329c28fc7e4ce3f9
Kimgyuilli <rlarbdlf222@naver.com>
2024-12-31 19:00:20 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -4,11 +4,14 @@ function loadItems() {
     .then(json => json.items);
 }
 
+// Updata the list with the givin items
 function displayItems(items) {
     const container = document.querySelector('.items');
+    // const html = items = items.map(item => createHTMLString(item));
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// Create HTML list item form the given item
 function createHTMLString(item){
     return `
     <li class="item">
@@ -18,11 +21,54 @@ function createHTMLString(item){
     `;
 }
 
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null){
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+// // invisible로 해결하는 힌트
+// function onButtonClick(event, items) {
+//     const target = event.target;
+//     const key = target.dataset.key;
+//     const value = target.dataset.value;
+//     if(key == null || value == null){
+//         return;
+//     }
+//     updateItems(items, key, value);
+// }
+
+// function updateItems(items, key, value) {
+//     items.forEach(item => {
+//         if(item.dataset[key] === value){
+//             item.classList.remove('invisible');
+//         } else {
+//             item.classList.add('invisible');
+//         }
+//     });
+// }
+
+
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 //main
 loadItems()
 .then(items => {
     console.log(items)
     displayItems(items);
-    // setEventListeners(items)
+    setEventListeners(items)
 })
 .catch(console.log)
