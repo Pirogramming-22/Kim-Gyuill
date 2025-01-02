d9a85dd2778675ef2bd8940e806a31b60b10a114
Kimgyuilli <rlarbdlf222@naver.com>
2025-01-03 00:53:28 +0900

    1번 메뉴 완성

--- pythone_problem/python_problem_2.py
+++ pythone_problem/python_problem_2.py
@@ -1,20 +1,24 @@
 #함수 이름은 변경 가능합니다.
 
-##############  menu 1
-def Menu1(#매개변수가 필요한지 판단 후 코딩할 것) :
-    #사전에 학생 정보 저장하는 코딩 
+# 학생 딕셔너리
+students = {}
 
-##############  menu 2
-def Menu2(#매개변수가 필요한지 판단 후 코딩할 것) :
-    #학점 부여 하는 코딩
+# menu 1: 학생 정보 저장
+def insert_student_info(name, mid, final) :
+    students[name] = {'mid' : mid, 'final' : final}
+    
 
-##############  menu 3
-def Menu3(#매개변수가 필요한지 판단 후 코딩할 것) :
-    #출력 코딩
+# ##############  menu 2
+# def Menu2(#매개변수가 필요한지 판단 후 코딩할 것) :
+#     #학점 부여 하는 코딩
 
-##############  menu 4
-def Menu4(#매개변수가 필요한지 판단 후 코딩할 것):
-    #학생 정보 삭제하는 코딩
+# ##############  menu 3
+# def Menu3(#매개변수가 필요한지 판단 후 코딩할 것) :
+#     #출력 코딩
+
+# ##############  menu 4
+# def Menu4(#매개변수가 필요한지 판단 후 코딩할 것):
+#     #학생 정보 삭제하는 코딩
 
 #학생 정보를 저장할 변수 초기화
 print("*Menu*******************************")
@@ -27,9 +31,29 @@ print("*************************************")
 while True :
     choice = input("Choose menu 1, 2, 3, 4, 5 : ")
     if choice == "1":
-        #학생 정보 입력받기
-        #예외사항 처리(데이터 입력 갯수, 이미 존재하는 이름, 입력 점수 값이 양의 정수인지)
-        #예외사항이 아닌 입력인 경우 1번 함수 호출 
+        try:
+            #학생 정보 입력받기
+            student_info = input("Enter name mid-score final-score : ").split()
+            
+            # 예외 처리 - 데이터 입력 갯수
+            if len(student_info != 3):
+                print("잘못된 입력입니다. 입력 예시) kim 100 80")
+                continue
+                
+            # 예외처리 - 이름 중복
+            name, mid_score, final_score = student_info
+            if name in students:
+                print("이미 존재하는 이름입니다.")
+                continue
+            
+            # 예외처리 점수가
+            if not mid_score.isdigit() or not final_score.isdigit():
+                print("점수는 양의 정수여야 합니다.")
+                continue
+                
+            insert_student_info(name, int(mid_score), int(final_score))
+        except Exception as e:
+            print(e)
 
     elif choice == "2" :
         #예외사항 처리(저장된 학생 정보의 유무)
@@ -51,4 +75,5 @@ while True :
         #반복문 종료
 
     else :
-        #"Wrong number. Choose again." 출력
+        print("Wrong number. Choose again.")
+        continue
