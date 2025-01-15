04c04a7974f755f30123a79821e82a6d1db4ff7c
Kimgyuilli <rlarbdlf222@naver.com>
2025-01-15 15:41:29 +0900

    [feat] 개발 툴 기본 기능까지 구현

--- SWIDEA_SITE/tools/forms.py
+++ SWIDEA_SITE/tools/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Tool  
 
-class UserForm(forms.ModelForm):
+class ToolForm(forms.ModelForm):
     class Meta:
         model = Tool
         fields = ('__all__')
--- SWIDEA_SITE/tools/urls.py
+++ SWIDEA_SITE/tools/urls.py
@@ -8,5 +8,19 @@ app_name = 'tools'
 
 urlpatterns = [    
 
+    # 메인
+    path('', tools_list, name='tools_list'),
+
+    # 폼 생성
+    path('create', tools_create, name='tools_create'),
+
+    # 폼 디테일
+    path('detail/<int:pk>', tools_detail, name='tools_detail'),
+    
+    # 폼 업데이트
+    path('update/<int:pk>', tools_update, name='tools_update'),
+
+    # 폼 삭제
+    path('delete/<int:pk>', tools_delete, name='tools_delete'),
 
 ]
