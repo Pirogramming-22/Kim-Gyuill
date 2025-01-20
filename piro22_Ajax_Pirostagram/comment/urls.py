8a23020db796cdf38d45ceec779c63c9f5e16fa1
Kimgyuilli <rlarbdlf222@naver.com>
2025-01-21 02:38:22 +0900

    [feat] 대댓글 구현

--- piro22_Ajax_Pirostagram/comment/urls.py
+++ piro22_Ajax_Pirostagram/comment/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.comment_page, name="comment_page"),
     path('<int:pk>/', views.comment_page, name="comment_page_with_pk"),
     path('delete/<int:pk>/', views.delete_comment, name='delete_comment'),
+    path('delete_reply/<int:pk>/', views.delete_reply, name='delete_reply'),
 ]
--- piro22_Ajax_Pirostagram/comment/views.py
+++ piro22_Ajax_Pirostagram/comment/views.py
@@ -4,11 +4,15 @@ from .models import Comment, Board
 
 def comment_page(request, pk):
     post = get_object_or_404(Board, id=pk)
-    comments = Comment.objects.filter(board=post).order_by('-created_at')
+    comments = Comment.objects.filter(board=post, parent__isnull=True).order_by('-created_at')  # 부모 댓글만 가져오기
     if request.method == 'POST':
         content = request.POST.get('content')
+        parent_id = request.POST.get('parent_id')
         if content:
-            Comment.objects.create(board=post, content=content)
+            parent_comment = None
+            if parent_id:
+                parent_comment = get_object_or_404(Comment, pk=parent_id)
+            Comment.objects.create(board=post, content=content, parent=parent_comment)
             return redirect('comment:comment_page_with_pk', pk=post.id)
     return render(request, 'comment.html', {'post': post, 'comments': comments})
 
@@ -19,3 +23,11 @@ def delete_comment(request, pk):
         board_pk = comment.board.id
         comment.delete()
         return redirect('comment:comment_page_with_pk', pk=board_pk)
+    
+
+def delete_reply(request, pk):
+    if request.method == "POST":
+        reply = get_object_or_404(Comment, pk=pk)
+        parent_comment = reply.parent
+        reply.delete()
+        return redirect('comment:comment_page_with_pk', pk=parent_comment.board.id)
